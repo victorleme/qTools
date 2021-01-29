695c9ab45e42e22c082c99a28c88645fcc140473
Victor Leme <victorlemefreitas@gmail.com>
2021-01-29 12:10:29 -0300

    Added layercake

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,41 @@
-<script>
-	export let name;
+<script lang="ts">
+  export let name;
+  let num: number = 0;
+
+  const addNumber = (i: number) => {
+    num = num + i;
+  };
+  type TodoType = {
+    id: number;
+    name: string;
+    completed: boolean;
+  };
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>qTools</h1>
+  <p>Quantum Finance Analytics App</p>
+  <button on:click={() => addNumber(1)}>Add number</button>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
